2d71597271c8a2d91f3d9dcfa2f891913d2ba612
Vicky-shah <waqas@iintellect.co.uk>
2021-08-19 18:59:35 +0500

    Added Scaling, Security and Improved Coding Standards

--- php/Block.php
+++ php/Block.php
@@ -63,28 +63,38 @@ class Block {
 	 */
 	public function render_callback( $attributes, $content, $block ) {
 		$post_types = get_post_types( [ 'public' => true ] );
-		$class_name = $attributes['className'];
+        $class_name = $attributes['className'];
+		$class_name = isset($attributes['className'])? isset($attributes['className']):'';
+		$class_name = apply_filters( 'filterposts_classname', $class_name );
+        $class_name = esc_attr__( $attributes['className'], 'site-counts' );
 		ob_start();
 
 		?>
 		<div class="<?php echo $class_name; ?>">
-			<h2>Post Counts</h2>
+			<h2><?php esc_html_e( 'Post Counts', 'site-counts' ); ?></h2>
 			<?php
-			foreach ( $post_types as $post_type_slug ) :
-				$post_type_object = get_post_type_object( $post_type_slug );
-				$post_count = count(
-					get_posts(
-						[
-							'post_type' => $post_type_slug,
-							'posts_per_page' => -1,
-						]
-					)
-				);
+			if(is_array($post_types)):
+				foreach ( $post_types as $post_type_slug ) :
+					$post_type_object = get_post_type_object( $post_type_slug );
+					$post_count = count(
+						get_posts(
+							[
+								'post_type'      => $post_type_slug,
+								'posts_per_page' => -1,
+							]
+						)
+					);
 
-				?>
-				<p><?php echo 'There are ' . $post_count . ' ' . $post_type_object->labels->name . '.'; ?></p>
-			<?php endforeach; ?>
-			<p><?php echo 'The current post ID is ' . $_GET['post_id'] . '.'; ?></p>
+					?>
+					<?php $posts_details = sprintf( esc_html__( 'There are  1%d. 2%s.', 'site-counts' ), intval( $post_count ), esc_html( $post_type_object->labels->name ) );
+							$posts_details = apply_filters( 'filter_postdetails', $posts_details );
+							$current_post_details = sprintf( esc_html__( 'The current post ID is %d.', 'site-counts' ), sanitize_text_field( wp_unslash( $_GET['post_id'] ) );
+							$current_post_details = apply_filters( 'fiter_currentpost_details', $current_post_details );
+					?>
+					<p><?php echo $posts_details;?> </p>
+				<?php endforeach; ?>
+				<?php echo $current_post_details;?>
+			<?php endif; ?>
 		</div>
 		<?php
 
